f762eeff2da87f225b34cc5967ed89a9e76f2b41
p-k-pavan <pkpavan2003@gmail.com>
2025-10-06 14:05:33 +0530

    MongoDB Connection

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL || "")
+.then(() => {
+    console.log("Connected to MongoDB");
+}).catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
