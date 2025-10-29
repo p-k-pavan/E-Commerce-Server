4197ff2523db957e6bcdf49684c3aa08545c9cd5
p-k-pavan <pkpavan2003@gmail.com>
2025-10-29 13:22:03 +0530

    reset password

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { forgotPassword, loginUser, logoutUser, registerUser, updateUser, verifyForgotPasswordOtp } from "../controllers/user.controller";
+import { forgotPassword, loginUser, logoutUser, registerUser, resetPassword, updateUser, verifyForgotPasswordOtp } from "../controllers/user.controller";
 import VerifyToken from "../middleware/VerifyToken";
 
 const router = express.Router();
@@ -9,6 +9,7 @@ router.get("/logout", logoutUser);
 router.put("/update", VerifyToken, updateUser);
 router.put("/forgot-password", forgotPassword);
 router.put("/verify-otp", verifyForgotPasswordOtp);
+router.put("/reset-password",resetPassword);
 
 
 export default router;
