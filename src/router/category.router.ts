978273580522caa07022dd6398fd580b2829b1b3
PAVAN KUMAR R <149031026+p-k-pavan@users.noreply.github.com>
2026-04-06 15:19:19 +0530

    Add bulk upload route for categories

--- src/router/category.router.ts
+++ src/router/category.router.ts
@@ -1,10 +1,11 @@
 import express from "express"
 import VerifyToken from "../middleware/VerifyToken";
-import { addCategory, deleteCategory, getCategory, updateCategory } from "../controllers/category.controller";
+import { addCategory, deleteCategory, getCategory, updateCategory,bulkUploadCategory } from "../controllers/category.controller";
 
 const router = express.Router();
 
 router.post("/",VerifyToken,addCategory);
+router.post("/bulk",VerifyToken,bulkUploadCategory);
 router.put("/:id",VerifyToken,updateCategory);
 router.delete("/:id",VerifyToken,deleteCategory);
 router.get("/",getCategory);
